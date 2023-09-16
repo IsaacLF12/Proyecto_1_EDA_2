04036fcf05c2c6f1db1632c1df8301e0e2f9936c
Sebjimort <112743352+Sebjimort@users.noreply.github.com>
2023-09-15 19:34:46 -0600

    Add files via upload

--- src/proyecto_1eda2/Ordenamientos.java
+++ src/proyecto_1eda2/Ordenamientos.java
@@ -36,4 +36,97 @@ public class Ordenamientos{
         }  
         return operations;
     }  
+    public static int bubbleSort(int[] array){
+        int operations = 0;
+        int z;
+        operations = operations+2;
+        for(int i=array.length-1; i>0; i--){
+            z = 0;
+            for(int j=0; j<i; j++){
+                if(array[j]>array[j+1]){
+                    Utilerias.swap(array, j, j+1);
+                    operations = operations+3;
+                }else{
+                    z++;
+                    operations = operations+1;
+                }
+                operations = operations+1;
+            }
+            if(z==i)
+                return operations+3;
+            operations = operations+3;
+        }
+        return operations;
+    }
+    public static int heapSort(int[] arr){
+        int operations = 0;
+        int n = arr.length;
+        operations = operations+3;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            operations = heapify(arr, n, i, operations);
+        for (int i = n - 1; i >= 0; i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            operations = heapify(arr, i, 0, operations);
+            operations = operations+3;
+        }
+        return operations;
+    }
+    public static int heapify(int[] arr, int n, int i, int operations){
+        int largest = i; 
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+            operations = operations+1;
+        }
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+            operations = operations+1;
+        }
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            operations = heapify(arr, n, largest, operations);
+            operations = operations+3;
+        }
+        operations = operations+6;
+        return operations;
+    }
+    public static int QuickSort(int arr[], int low, int high){
+        int[] pi_operations = new int[2];
+        pi_operations[0] = 0;
+        pi_operations[1] = 0;
+        Quick(arr, low, high, pi_operations);
+        return pi_operations[1];
+    }
+    public static void Quick(int arr[], int low, int high, int pi_operations[]){
+        if (low < high){
+            partition(arr, low, high, pi_operations);
+            Quick(arr, low, pi_operations[0]-1, pi_operations);
+            Quick(arr, pi_operations[0]+1, high, pi_operations);
+        }
+        pi_operations[1] = pi_operations[1]+1;
+    }
+    public static void partition(int arr[], int low, int high, int pi_operations[]){
+        int pivot = arr[high];
+        pi_operations[0] = (low-1);
+        for (int j=low; j<high; j++){
+            if (arr[j] <= pivot){
+                pi_operations[0]++;
+                Utilerias.swap(arr, pi_operations[0],j);
+                pi_operations[1] = pi_operations[1]+4;
+            }
+            pi_operations[1] = pi_operations[1]+1;
+        }
+        int temp = arr[pi_operations[0]+1];
+        arr[pi_operations[0]+1] = arr[high];
+        arr[high] = temp;
+        pi_operations[0]++;
+        pi_operations[1] = pi_operations[1]+7;
+    }
+    
+
 }
--- src/proyecto_1eda2/Principal.java
+++ src/proyecto_1eda2/Principal.java
@@ -6,8 +6,8 @@ public class Principal{
         int[] arr = new int[10000];
         for(int i=0; i<arr.length; i++)
         //    arr[i] = i;
-        //   arr[i] = aleatorio.nextInt(1000);
-            arr[i] = arr.length-i;
+           arr[i] = aleatorio.nextInt(1000);
+        //    arr[i] = arr.length-i;
         System.out.println("Este es el arreglo:");
         Utilerias.printArray(arr);
         System.out.println("El numero de operaciones fue: " + Ordenamientos.QuickSort(arr, 0, arr.length-1));
